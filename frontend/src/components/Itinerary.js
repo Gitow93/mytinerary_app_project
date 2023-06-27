125d9b36d5ac64179977813b3fde6ec55d0ea03e
Jorge <jorge.jimenezmunoz.93@gmail.com>
2023-06-27 18:13:37 +0200

    add itineraries fetch and render itineraries

--- frontend/src/components/Itinerary.js
+++ frontend/src/components/Itinerary.js
@@ -4,18 +4,35 @@ import axios from "axios";
 
 const Itinerary = () => {
   const { id } = useParams();
+  const [itinerary, setItinerary] = React.useState([]);
 
   const fetchData = async () => {
     const data = await axios.get(`http://localhost:5000/itinerary/${id}`);
+    setItinerary(data.data);
   };
 
   useEffect(() => {
     fetchData();
+    console.log("hola");
   }, []);
+
+  useEffect(() => {
+    console.log(itinerary);
+  }, [itinerary]);
 
   return (
     <div>
       <h1>Itineraries</h1>
+      {
+        <ul>
+          {itinerary?.map((itineraryInfo) => (
+            <React.Fragment key={itineraryInfo._id}>
+              <li>{itineraryInfo.title}</li>
+              <li>{itineraryInfo.rating}</li>
+            </React.Fragment>
+          ))}
+        </ul>
+      }
     </div>
   );
 };
